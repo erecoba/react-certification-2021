ecd8f89c53e2624aca40d906fe21c39a7853b9b1
Jose Erubiel Recoba <erubiel.recoba@wizeline.com>
2021-03-21 16:00:10 -0600

    Cambia el api hardcoded por .env por cuestiones de vulnerabilidad

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,20 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './global.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function start() {
+  // eslint-disable-next-line
+  gapi.client
+    .init({
+      apiKey: process.env.REACT_APP_YOUTUBE_API,
+    })
+    .then(() => {
+      ReactDOM.render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>,
+        document.getElementById('root')
+      );
+    });
+}
+// eslint-disable-next-line
+gapi.load('client', start);
